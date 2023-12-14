b99943e44d4ced4660959de73dc9e8dd6db83825
artyNinjaP <siyongnin1993@gmail.com>
2023-12-14 22:10:57 +0700

    summit edit a lot lot

--- nodeserver.js
+++ nodeserver.js
@@ -2,14 +2,19 @@ const express = require('express')
 const app = express()
 const port = 8800
 
+//Connect public folder เชื้อมต่อไฟล์ css และก็ image
+app.use(express.static('public'))
+
+
 app.set('view engine','ejs')
 
 
 app.get("/",(req,res) =>{ 
     res.render('index')})
 
+app.get('/properties', (req, res) => {
+  res.render('proterties')
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
